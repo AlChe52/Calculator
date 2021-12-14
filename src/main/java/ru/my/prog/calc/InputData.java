16a117a6d477f36e4536d3df73832a60e693ff46
AlChe52 <fastshadow@inbox.ru>
2021-12-14 15:55:32 +0300

    change method isRight, need add check for another symbol

--- src/main/java/ru/my/prog/calc/InputData.java
+++ src/main/java/ru/my/prog/calc/InputData.java
@@ -24,10 +24,26 @@ class InputData {
 
    private boolean isRight (String data) {
      char[] check = data.toCharArray();
+       for (int i = 1; i <check.length; i++) {
+           if (check[i-1]=='+' && check[i]=='+' ||
+               check [i-1]=='-' && check[i]=='-' ||
+                   check [i-1]=='*' && check[i]=='*' ||
+                   check [i-1]=='/' && check[i]=='/') {
+               pc.print("Sorry, in your expression have ");
+           return false;
+           }
+           if (check[i-1]=='/' && check[i]=='0') {
+               pc.print("Sorry, we don't support divide by zero ");
+               return false;
+           }
+
+
+       }
      if(check[0]=='-') {
          pc.print("Sorry, program doesn't work with negative numbers, wait for an update");
          return false;
      }
+
 
      return true;
      }
